da2643e64037d67db0e6e7d3283743e81ffe4186
Ben Smith <93447202+ben5049@users.noreply.github.com>
2023-10-17 21:39:39 +0100

    Added "Calibrated_IMU_output"

---
+++ examples/Calibrated_IMU_output/Calibrated_IMU_output.ino
@@ -0,0 +1,84 @@
+// "Calibrated_sensor_output.ino" from the FastIMU library adapted for the ICRS 101 Devkit.
+//
+// This program prints out the x, y and z values for the gyroscope and accelerometer built into the board.
+
+#include "ICRS_101.h"
+#include "FastIMU.h"
+#include <Wire.h>
+
+#define IMU_ADDRESS 0x6A    //Change to the address of the IMU
+#define PERFORM_CALIBRATION //Comment to disable startup calibration
+LSM6DSL IMU;               //Creates an object representing the IMU
+
+calData calib = { 0 };  //Calibration data
+AccelData accelData;    //Sensor data
+GyroData gyroData;
+
+void setup() {
+  Wire.begin();
+  Serial.begin(115200);
+  while (!Serial) {
+    ;
+  }
+
+  int err = IMU.init(calib, IMU_ADDRESS);
+  if (err != 0) {
+    Serial.print("Error initializing IMU: ");
+    Serial.println(err);
+    while (true) {
+      ;
+    }
+  }
+  
+#ifdef PERFORM_CALIBRATION
+  Serial.println("FastIMU calibration & data example");
+  delay(1000);
+  Serial.println("Keep IMU level.");
+  delay(5000);
+  IMU.calibrateAccelGyro(&calib);
+  Serial.println("Calibration done!");
+  Serial.println("Accel biases X/Y/Z: ");
+  Serial.print(calib.accelBias[0]);
+  Serial.print(", ");
+  Serial.print(calib.accelBias[1]);
+  Serial.print(", ");
+  Serial.println(calib.accelBias[2]);
+  Serial.println("Gyro biases X/Y/Z: ");
+  Serial.print(calib.gyroBias[0]);
+  Serial.print(", ");
+  Serial.print(calib.gyroBias[1]);
+  Serial.print(", ");
+  Serial.println(calib.gyroBias[2]);
+  delay(5000);
+  IMU.init(calib, IMU_ADDRESS);
+#endif
+
+  //err = IMU.setGyroRange(500);      //USE THESE TO SET THE RANGE, IF AN INVALID RANGE IS SET IT WILL RETURN -1
+  //err = IMU.setAccelRange(2);       //THESE TWO SET THE GYRO RANGE TO ±500 DPS AND THE ACCELEROMETER RANGE TO ±2g
+  
+  if (err != 0) {
+    Serial.print("Error Setting range: ");
+    Serial.println(err);
+    while (true) {
+      ;
+    }
+  }
+}
+
+void loop() {
+  IMU.update();
+  IMU.getAccel(&accelData);
+  Serial.print(accelData.accelX);
+  Serial.print("\t");
+  Serial.print(accelData.accelY);
+  Serial.print("\t");
+  Serial.print(accelData.accelZ);
+  Serial.print("\t");
+  IMU.getGyro(&gyroData);
+  Serial.print(gyroData.gyroX);
+  Serial.print("\t");
+  Serial.print(gyroData.gyroY);
+  Serial.print("\t");
+  Serial.print(gyroData.gyroZ);
+  delay(50);
+}
