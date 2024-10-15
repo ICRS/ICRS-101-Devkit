13d0403ec83593d41432b4ee7f0267a997b0f3a5
Ben Smith <bens15351@gmail.com>
2024-10-16 00:59:11 +0100

    Changed from D3 to BUILTIN_LED

--- examples/GPIO/Fade/Fade.ino
+++ examples/GPIO/Fade/Fade.ino
@@ -1,21 +1,17 @@
-#include "ICRS_101.h"
-
-/* LED Fade example: Digital PWM for GPIO Pins  */
+/* LED Fade example: Digital PWM for GPIO Pins */
 
 void setup() {
-  pinMode(D3, OUTPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
 }
 
 void loop() {
-  for(int i = 0; i < 255; i++)
-  {  
-    analogWrite(D3, i);
-    delay(50);
+  for (int i = 0; i < 255; i++) {  
+    analogWrite(BUILTIN_LED, i);
+    delay(5);
   }
 
-  for(int i = 255; i > 0; i--)
-  {  
-    analogWrite(D3, i);
-    delay(50);
+  for (int i = 255; i > 0; i--) {  
+    analogWrite(BUILTIN_LED, i);
+    delay(5);
   }
 }
