e82eeb1980ec42669a93ed5cf97f57881c54af6e
maxryan4 <161385834+maxryan4@users.noreply.github.com>
2025-01-19 10:35:56 +0000

    updated blink to work with esp32 c3 super mini & updated helloworld script

--- examples/Blink/Blink.ino
+++ examples/Blink/Blink.ino
@@ -1,15 +1,17 @@
 /* Program to blink the built-in LED once per second */
 
+#define LED_BUILTIN 8 // normally not necessary
+
 /* the setup function runs once when you press reset or power the board */
 void setup() {
   /* initialize LED_BUILTIN pin as an output */
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-/* the loop function runs over and over again forever */
+// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH); /* turn the LED on */
-  delay(500);                      /* wait for half a second */
-  digitalWrite(LED_BUILTIN, LOW);  /* turn the LED off */
-  delay(500);                      /* wait for half a second */
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on
+  delay(500);                      // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off
+  delay(500);                      // wait for half a second
 }
--- examples/Fade/Fade.ino
+++ examples/Fade/Fade.ino
@@ -1,10 +1,13 @@
 /* LED Fade example: Digital PWM for GPIO Pins */
+#define BUILTIN_LED 8
 
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
+  Serial.println("Moving Forward");
   for (int i = 0; i < 255; i++) {  
     analogWrite(BUILTIN_LED, i);
     delay(5);
