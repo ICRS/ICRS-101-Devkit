96b9f5cc53558d0ead6dc05a7a604189a114bcd2
Max Ryan <161385834+maxryan4@users.noreply.github.com>
2025-01-23 17:02:40 +0000

    Fixed button code

--- examples/Button/Button.ino
+++ examples/Button/Button.ino
@@ -1,10 +1,11 @@
 /* Program to print button status and turns on LED if button pressed */
 #define LED_BUILTIN 8
+#define BOOT_BUTTON 9
 
 /* The setup function runs once when you press reset or power the board */
 void setup() {
   Serial.begin(115200);
-  pinMode(0, INPUT_PULLUP);
+  pinMode(BOOT_BUTTON, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 }
@@ -12,9 +13,7 @@ void setup() {
 /* The loop function runs repeatedly */
 void loop() {
 
-  bool status = digitalRead(0) == LOW;
-
-  if (digitalRead(0) == LOW) {
+  if (digitalRead(BOOT_BUTTON) == LOW) {
     Serial.println("Button has been pressed!");
     digitalWrite(LED_BUILTIN, 1);
     delay(10);
