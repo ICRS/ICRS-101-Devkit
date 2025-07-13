1b0dc1c855c62d729f7ec0a678600fccfd26c3be
Max Ryan <161385834+maxryan4@users.noreply.github.com>
2025-07-13 15:31:28 +0100

    Updated code

--- examples/MotorControl/MotorControl.ino
+++ examples/MotorControl/MotorControl.ino
@@ -1,23 +1,16 @@
-/* 
-do yourself
-get motors moving so it moves: 
-forward for 0.5s
-backwards for 0.5s
-stop for 0.5s
-repeat
-*/
+// Motor A (left) pins
+const int leftPin1 = 3;  // MOTORA1
+const int leftPin2 = 4;  // MOTORA2
 
-
-// Motor right pins
-const int rightPin1 = 4;
-const int rightPin2 = 3;
-
-// Motor left pins
-const int leftPin1 = 2;
-const int leftPin2 = 1;
+// Motor B (right) pins
+const int rightPin1 = 1; // MOTORB1
+const int rightPin2 = 2; // MOTORB2
 
 void setup() {
-  //TODO: Set motor control pins as outputs
+  pinMode(rightPin1, OUTPUT);
+  pinMode(rightPin2, OUTPUT);
+  pinMode(leftPin1, OUTPUT);
+  pinMode(leftPin2, OUTPUT);
 
   Serial.begin(115200);
 }
@@ -25,29 +18,54 @@ void setup() {
 void loop() {
   moveForward();
   delay(500);
+  turnLeft();
+  delay(500);
+  turnRight();
+  delay(500);
   moveBackward();
   delay(500);
   stopMotors();
-  delay(500);
+  delay(5000);
 }
 
 // Functions to control the motors
+// Functions to control the motors
 void moveForward() {
   Serial.println("Moving Forward");
-  // TODO: Set motor control pins to move the robot forward
-
+  digitalWrite(leftPin1, HIGH);
+  digitalWrite(leftPin2, LOW);
+  digitalWrite(rightPin1, HIGH);
+  digitalWrite(rightPin2, LOW);
 }
 
 void moveBackward() {
   Serial.println("Moving Backward");
-  // TODO: Set motor control pins to move the robot backward
-  
+  digitalWrite(leftPin1, LOW);
+  digitalWrite(leftPin2, HIGH);
+  digitalWrite(rightPin1, LOW);
+  digitalWrite(rightPin2, HIGH);
+}
+
+void turnRight() {
+  Serial.println("Turning Right");
+  digitalWrite(leftPin1, HIGH);
+  digitalWrite(leftPin2, LOW);
+  digitalWrite(rightPin1, LOW);
+  digitalWrite(rightPin2, HIGH);
+}
+
+void turnLeft() {
+  Serial.println("Turning Left");
+  digitalWrite(leftPin1, LOW);
+  digitalWrite(leftPin2, HIGH);
+  digitalWrite(rightPin1, HIGH);
+  digitalWrite(rightPin2, LOW);
 }
 
 void stopMotors() {
   Serial.println("Stopping Motors");
-  digitalWrite(rightPin1, LOW);
-  digitalWrite(rightPin2, LOW);
   digitalWrite(leftPin1, LOW);
   digitalWrite(leftPin2, LOW);
+  digitalWrite(rightPin1, LOW);
+  digitalWrite(rightPin2, LOW);
 }
